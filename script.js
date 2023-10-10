1304037eb86354b965eb93b063dc378289b97a2a
Dev-kami <blossomobasi2@gmail.com>
2023-10-10 11:40:30 +0100

    Changed image

---
+++ script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //   Auto Slide
+  autoplay: {
+    delay: 5000,
+  },
+  effect: "cube",
+  cubeEffect: {
+    slideShadows: false,
+  },
+});
